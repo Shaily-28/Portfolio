38e8f8e8fe6f9d6de1eecf7c3f040169908b562a
Shaily-28 <snievesadame@ucsd.edu>
2025-10-13 15:43:20 -0700

    Automatic current page

--- global.js
+++ global.js
@@ -2,3 +2,6 @@ console.log("IT'S ALIVE!")
 export function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 } 
+const navLinks = Array.from(document.querySelectorAll("nav a"));
+const current = navLinks.find(a => a.host === location.host && a.pathname === location.pathname);
+current?.classList.add("current");
