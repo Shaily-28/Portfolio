d2138457385d9a571655abf5a4948648974f877f
Shaily-28 <snievesadame@ucsd.edu>
2025-11-01 16:22:22 -0700

    Drawing circle path

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,5 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 import { fetchJSON, renderProjects } from '../global.js';
 
 async function initProjectsPage() {
@@ -18,3 +20,11 @@ async function initProjectsPage() {
 }
 
 initProjectsPage();
+
+const svg = d3.select('#projects-pie-plot');
+
+const arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+const arc = arcGenerator({ startAngle: 0, endAngle: 2 * Math.PI });
+
+svg.append('path').attr('d', arc).attr('fill', 'red');
