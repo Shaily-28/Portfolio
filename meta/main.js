8426810fd3c14f968e6b2a93061ae0b2c525d2d0
Shaily-28 <snievesadame@ucsd.edu>
2025-11-06 22:07:09 -0800

    created summary stats/visualizing time and commits.

--- meta/main.js
+++ meta/main.js
@@ -64,6 +64,70 @@ function renderCommitInfo(data, commits) {
   dl.append('dt').text('Maximum depth');
   dl.append('dd').text(maxDepth ?? 0);
 }
+function renderScatterPlot(data, commits) {
+  const width = 600;
+  const height = 350;
+  const margin = { top: 10, right: 10, bottom: 40, left: 50 };
+
+  const svg = d3.select('#chart').append('svg').attr('viewBox', `0 0 ${width} ${height}`).style('overflow', 'visible');
+
+  const usable = {
+    left: margin.left,
+    right: width - margin.right,
+    top: margin.top,
+    bottom: height - margin.bottom,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(commits, d => d.datetime))
+    .range([usable.left, usable.right])
+    .nice();
+
+  const yScale = d3.scaleLinear()
+    .domain([0, 24])
+    .range([usable.bottom, usable.top]);
+
+
+  svg.append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usable.left},0)`)
+    .call(d3.axisLeft(yScale).tickFormat('').tickSize(-usable.width));
+
+ 
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale)
+    .tickFormat(d => String(d % 24).padStart(2, '0') + ':00');
+
+  svg.append('g')
+    .attr('transform', `translate(0,${usable.bottom})`)
+    .call(d3.axisBottom(xScale).ticks(6)); // ← fewer ticks
+
+  svg.append('g')
+    .attr('transform', `translate(${usable.left},0)`)
+    .call(d3.axisLeft(yScale).tickFormat(d => String(d % 24).padStart(2, '0') + ':00'));
+
+  
+  const r = 3;               
+  const jitterHours = 0.35;   
+  const jitterMinutes = 18;   
+  svg.append('g')
+    .attr('class', 'dots')
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', d => {
+      const ms = (Math.random() - 0.5) * jitterMinutes * 60 * 1000;
+      return xScale(new Date(d.datetime.getTime() + ms));
+    })
+    .attr('cy', d => yScale(d.hourFrac + (Math.random() - 0.5) * jitterHours))
+    .attr('r', r)
+    .attr('fill', 'steelblue')
+    .attr('fill-opacity', 0.65) 
+    .attr('stroke', '#fff')     
+    .attr('stroke-width', 0.75);
+}
 
 (async function init() {
   const data = await loadData();
@@ -71,3 +135,4 @@ function renderCommitInfo(data, commits) {
   renderCommitInfo(data, commits);
   renderScatterPlot(data, commits); 
 })();
+
